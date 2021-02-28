16e52acd1f6a9a1a3f1f0f441e31124030889362
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-01 00:06:35 +0530

    sched: Add branch predictor hints to battery saver checks
    * optimize these as much as we can

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -762,7 +762,7 @@ int schedtune_task_boost(struct task_struct *p)
 	struct schedtune *st;
 	int task_boost;
 
-	if (!unlikely(schedtune_initialized) || is_battery_saver_on())
+	if (!unlikely(schedtune_initialized) || unlikely(is_battery_saver_on()))
 		return 0;
 
 	/* Get task boost value */
@@ -779,7 +779,7 @@ int schedtune_prefer_idle(struct task_struct *p)
 	struct schedtune *st;
 	int prefer_idle;
 
-	if (!unlikely(schedtune_initialized) || is_battery_saver_on())
+	if (!unlikely(schedtune_initialized) || unlikely(is_battery_saver_on()))
 		return 0;
 
 	/* Get prefer_idle value */
@@ -796,7 +796,7 @@ prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
 
-	if (is_battery_saver_on())
+	if (unlikely(is_battery_saver_on()))
 		return 0;
 
 	return st->prefer_idle;
@@ -817,7 +817,7 @@ boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
 
-	if (is_battery_saver_on())
+	if (unlikely(is_battery_saver_on()))
 		return 0;
 
 	return st->boost;
